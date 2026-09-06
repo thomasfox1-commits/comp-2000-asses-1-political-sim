2d336cec0b633340d71ef7c7bba188bd261d01f9
thomasfox1-commits <Thomas.fox1@students.mq.edu.au>
2026-09-06 22:35:50 +1000

    Implemnting closing functionality to the canvas to close  program when the windows closed

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,9 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 public class Canvas extends JFrame{
     public Canvas(){
         setSize(1000,1000);
         setTitle("politicalSim");
         setLayout(null);
-        setVisible(true);        
+        setVisible(true);
+        
+        addWindowListener(new WindowAdapter(){
+            @Override 
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        }  );     
     }
 }
