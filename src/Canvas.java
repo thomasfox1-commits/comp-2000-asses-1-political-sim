85384e9aaa6c612f05be1857f410c8469f17af22
thomasfox1-commits <Thomas.fox1@students.mq.edu.au>
2026-09-06 22:19:16 +1000

    implementing constructer presentation logic to Canvas

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,4 +1,9 @@
 import javax.swing.*;
 public class Canvas extends JFrame{
-
+    public Canvas(){
+        setSize(1000,1000);
+        setTitle("politicalSim");
+        setLayout(null);
+        setVisible(true);        
+    }
 }
