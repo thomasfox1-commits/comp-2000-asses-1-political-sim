83c84b8916ff6f11b7e603652ce50121e1350fbb
tom fox <thomas.fox1@students.mq.edu.au>
2026-09-07 11:12:41 +1000

    implemented constructor for Land class and amended  Elf misextending Land

--- src/Elf.java
+++ src/Elf.java
@@ -1,3 +1,3 @@
-public class Elf extends Land{
+public class Elf extends Empire{
 
 }
--- src/Land.java
+++ src/Land.java
@@ -1,4 +1,31 @@
 import javax.swing.*;
-abstract class Land extends JPanel {
-
+abstract class  Land<Tile extends Land>  extends JPanel  {
+    int ID;
+    Tile left,right,above,below;
+    public Land(){
+        ID=0;
+        left=null;
+        right=null;
+        above=null;
+        below=null;
+    }
+    public Land(int identifier){
+        ID=identifier;
+        left=null;
+        right=null;
+        above=null;
+        below=null;
+    }
+    public void setRight(Tile land){
+        right=land;
+    }
+    public void setLeft(Tile land){
+        left=land;
+    }
+    public void setAbove(Tile land){
+        above=land;
+    }
+    public void setBelow(Tile land){
+        right=land;
+    }
 }
