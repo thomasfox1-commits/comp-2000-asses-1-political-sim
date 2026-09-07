3e3364c467d9fe29423b1a5ced6cea19c5c9539b
tom fox <thomas.fox1@students.mq.edu.au>
2026-09-07 13:13:56 +1000

    implemented treemap functioanlity with Grid tiles

--- src/Grid.java
+++ src/Grid.java
@@ -1,23 +1,42 @@
 import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 public class Grid extends JPanel{
-    public Grid(int width,int height, int gridSize){
-        Forest placeHolder;
+    TreeMap<Integer,Land> gameMap;
+    int gridSize;
+    public <E extends Land> Grid(int width,int height, int gridSize){
+        gameMap = new TreeMap<Integer,Land>();
+        this.gridSize = gridSize;
         setSize(width,height);
-        setLayout(null);
-        int tileSizeHeight,tileSizeWidth;
-        tileSizeWidth=width/gridSize;
-        tileSizeHeight=height/gridSize;
-        System.out.println("grid tile Width: " + tileSizeWidth + " Height: " + tileSizeHeight);
+        setLayout(null);;
+        //System.out.println("grid tile Width: " + tileSizeWidth + " Height: " + tileSizeHeight);
+        createGrid();
+
+        
+    }
+    public void createGrid(){
+        int tileSizeHeight,tileSizeWidth, tileID;
+        tileSizeWidth=getWidth()/gridSize;
+        tileSizeHeight=getHeight()/gridSize;
+        tileID=1;
+        Forest placeHolder;
         for(int x = 0;x<gridSize;x++){
             for(int y = 0;y<gridSize;y++){
                 placeHolder=new Forest();
+                placeHolder.ID=tileID;
                 placeHolder.setBackground(Color.black);
                 placeHolder.setSize(tileSizeWidth-1, tileSizeHeight-1);
                 placeHolder.setLocation(x*tileSizeWidth , y*tileSizeHeight);
                 add(placeHolder);
+                addTile(tileID,placeHolder);
+                tileID++;
             }
         }
+        gameMap.get(4).setBackground(Color.BLUE);
+
+    }
+    public void addTile(int identifier, Land land){
+        gameMap.put(identifier, land);
     }
     
 
