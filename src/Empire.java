28c801b6925ee5bf5b6e584917e476ae5d03d261
thomasfox1-commits <Thomas.fox1@students.mq.edu.au>
2026-09-10 00:00:48 +1000

    implemented an is owner method to check tiles belong to empire

--- src/Empire.java
+++ src/Empire.java
@@ -7,8 +7,32 @@ public class Empire {
         ownedLand= new ArrayList<>();
         nationColor = Color.white;
     }
+
     public Empire(Color col){
         ownedLand= new ArrayList<>();
         nationColor = col;
+    }
+
+    public ArrayList<Land> getBorderTiles(){
+        ArrayList<Land> list= new ArrayList<>();
+        if (ownedLand==null){
+            return null;
+        }
+        for(Land l:ownedLand){
+            
+        }
+        return list;
+    }
+
+    public Boolean isOwner(Land tile){
+        if (ownedLand==null){
+            return false;
+        }
+        for(Land l:ownedLand){
+            if(l==tile){
+                return true;
+            }
+        }
+        return false;
     }
 }
