54a38959ac0e00c36d3e0381554511712bee08f4
TNHgit <thomas.naglehill@students.mq.edu.au>
2026-09-12 02:20:56 +1000

    Basic Next Turn Button

--- src/Canvas.java
+++ src/Canvas.java
@@ -3,6 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 public class Canvas extends JFrame{
     Grid gameGrid;
+    JButton turnButton;
     Color white = new Color(207,207,207);
     Color darkGrey = new Color(110,110,110);
     public Canvas(){
@@ -15,9 +16,14 @@ public class Canvas extends JFrame{
         gameGrid.setLocation(0,0);
         gameGrid.setBackground(darkGrey);
         add(gameGrid);
+        turnButton = new JButton("Next Turn");
+        turnButton.setSize(300,100);
+        turnButton.setVisible(true);
+        turnButton.setLocation(300,650);
+        turnButton.addActionListener(l -> nextTurnPressed(gameGrid));
+        add(turnButton);
         setUpWindowCloser();
         setVisible(true);
-             
     }
     public Canvas(Grid grid, int width, int height, String title){
         setSize(width,height);
@@ -26,6 +32,12 @@ public class Canvas extends JFrame{
         setLayout(null);
         gameGrid= grid;
         add(gameGrid);
+        turnButton = new JButton("Next Turn");
+        turnButton.setSize(300,100);
+        turnButton.setVisible(true);
+        turnButton.setLocation(300,650);
+        turnButton.addActionListener(l -> nextTurnPressed(gameGrid));
+        add(turnButton);
         setUpWindowCloser();
         setVisible(true);
     }
@@ -37,5 +49,10 @@ public class Canvas extends JFrame{
                 System.exit(0);
             }
         }  );
+    }
+
+    public void nextTurnPressed(Grid gameGrid)
+    {
+        gameGrid.updateTurn();
     }
 }
