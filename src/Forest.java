0036d5c6ece3bc28fcf49e949fc7228aff551dae
Amy Calman <amy.calman@students.mq.edu.au>
2026-09-17 22:51:00 +1000

    started outlining the forest drawLand method

--- src/Forest.java
+++ src/Forest.java
@@ -1,5 +1,12 @@
+import java.awt.Graphics;
+
 public class Forest extends Land implements Improvable{
     private double growthBonus;
+    int[] xPoints = {5, 30, 50, 70, 95};
+    int[] yPoints = {75, 15, 60, 15, 75};
+    int[] firstRect = {25, 100};
+    int[] secondRect = {65, 100};
+    int[] rectSize = {10, 25};
     
     public Forest(){
         growthBonus = 0.2;
@@ -14,8 +21,14 @@ public class Forest extends Land implements Improvable{
     }
 
     @Override
-    public void drawLand() {
+    public void drawLand(int width, int height) {
         // TODO Auto-generated method stub
-        super.drawLand();
+        super.drawLand(width, height);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        // TODO Auto-generated method stub
+        super.paintComponent(g);
     }
 }
