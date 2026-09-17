b3caeab1d89998b1254395771b301fcbee1dce6a
Amy Calman <amy.calman@students.mq.edu.au>
2026-09-17 22:37:20 +1000

    slightly changed mountain drawing co-ords and fixed forest drawLand override

--- src/Mountain.java
+++ src/Mountain.java
@@ -3,11 +3,10 @@ import java.awt.Graphics;
 
 public class Mountain extends Land implements Impassable {
 
-    int[] xPoints = {10, 35, 50, 60, 90};
-    int[] yPoints = {95, 15, 50, 10, 95};
+    int[] xPoints = {10, 35, 48, 60, 90};
+    int[] yPoints = {95, 20, 50, 10, 95};
     @Override
     public void drawLand(int width, int height) {
-        // TODO Auto-generated method stub
         super.drawLand(width, height);
         for(int i = 0; i < xPoints.length; i++){
             xPoints[i] *= width;
@@ -15,7 +14,6 @@ public class Mountain extends Land implements Impassable {
             yPoints[i] *= height;
             yPoints[i] /= 100;
         }
-        System.out.println(xPoints[0] + ", " + yPoints[0]);
         repaint();
     }
 
